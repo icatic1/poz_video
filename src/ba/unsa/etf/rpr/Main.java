cc589389e15d2f09f36c668e136c05de08add2c5
Irvin Ćatić <icatic1@etf.unsa.ba>
2020-10-30 11:13:10 +0100

    Moj commitment

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -3,6 +3,10 @@ package ba.unsa.etf.rpr;
 public class Main {
 
     public static void main(String[] args) {
-	        System.out.println("Izmjena");
+
+        System.out.println("Izmjena");
+
+        System.out.println("Evo da i ja dodam svoj dio");
+        System.out.println( 11 );
     }
 }
